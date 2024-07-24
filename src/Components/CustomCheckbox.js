8a9599205519de0c5c27ac89bbf7240d9bf84283
Abdullah Şahin <99759896+AbdullahSahin1@users.noreply.github.com>
2024-07-24 22:11:38 +0300

    Update CustomCheckbox.js

--- src/Components/CustomCheckbox.js
+++ src/Components/CustomCheckbox.js
@@ -1,1 +1,22 @@
+import { useField } from "formik";
+import React, { Fragment } from "react";
+
+function CustomCheckbox({ label, ...props }) {
+  const [field, meta] = useField(props);
+  return (
+    <Fragment>
+      <div className="checkbox">
+        <input
+          {...field}
+          {...props}
+          className={meta.error ? "input-error" : ""}
+        />
+        <span>Kullanım koşullarını kabul ediyorum</span>
+      </div>
+      {meta.error && <div className="error">{meta.error}</div>}
+    </Fragment>
+  );
+}
+
+export default CustomCheckbox;
 

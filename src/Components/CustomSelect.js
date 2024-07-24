4285e77dc03b15304596d8ebdcdaacd90279109f
Abdullah Şahin <99759896+AbdullahSahin1@users.noreply.github.com>
2024-07-24 22:10:38 +0300

    Update CustomSelect.js

--- src/Components/CustomSelect.js
+++ src/Components/CustomSelect.js
@@ -1,1 +1,20 @@
+import { useField } from "formik";
+import React, { Fragment } from "react";
+
+function CustomSelect({ label, ...props }) {
+  const [field, meta] = useField(props);
+  return (
+    <Fragment>
+      <label>{label}</label>
+      <select
+        {...field}
+        {...props}
+        className={meta.error ? "input-error" : ""}
+      />
+      {meta.error && <div className="error">{meta.error}</div>}
+    </Fragment>
+  );
+}
+
+export default CustomSelect;
 

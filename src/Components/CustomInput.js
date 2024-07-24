6d5120abe96204a3ce01888aa10de4ef755aab2d
Abdullah Şahin <99759896+AbdullahSahin1@users.noreply.github.com>
2024-07-24 22:14:38 +0300

    Update CustomInput.js

--- src/Components/CustomInput.js
+++ src/Components/CustomInput.js
@@ -1,16 +1,19 @@
-import { useField } from 'formik'
-import React, { Fragment } from 'react'
+import { useField } from "formik";
+import React, { Fragment } from "react";
 
-function CustomInput({label,...props}) {
-    const [field,meta] = useField(props)
+function CustomInput({ label, ...props }) {
+  const [field, meta] = useField(props);
   return (
     <Fragment>
-        <label>{label}</label>
-        <input {...field} {...props} className={meta.error ? 'input-error':''}/>
-        {meta.error && <div className='error'>{meta.error}</div>}
+      <label>{label}</label>
+      <input
+        {...field}
+        {...props}
+        className={meta.error ? "input-error" : ""}
+      />
+      {meta.error && <div className="error">{meta.error}</div>}
     </Fragment>
-    
-  )
+  );
 }
 
-export default CustomInput
+export default CustomInput;

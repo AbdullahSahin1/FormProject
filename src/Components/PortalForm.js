6522e9c19230705e4c482985730c4c739f0a62f2
Abdullah Şahin <99759896+AbdullahSahin1@users.noreply.github.com>
2024-07-24 21:45:45 +0300

    Update PortalForm.js

--- src/Components/PortalForm.js
+++ src/Components/PortalForm.js
@@ -1,11 +1,29 @@
-import React from 'react'
-
+import React from 'react';
+import {Form , Formik} from 'formik'
+import CustomInput from './CustomInput';
+import { advancedSchema } from '../schemas';
+const onSubmit = async (values,actions) =>{
+  await new Promise((resolve)=>{
+    setTimeout(resolve,1000)
+  })
+  actions.resetForm()
+}
 function PortalForm() {
-  return (
-    <div>
-      
-    </div>
-  )
+  return <>
+      {' '}
+     <Formik
+       initialValues={{ userName: '', University: '', isAccepted: false }}
+       onSubmit= {onSubmit}
+       validationSchema={advancedSchema}
+     >
+       {() => (
+         <Form>
+           <CustomInput label = "kullanıcı adı"  name = "userName" type="text" placeholder= "Kullanıcı adınızı giriniz"/>
+          
+         </Form>
+       )}
+     </Formik>
+  </>;
 }
 
-export default PortalForm
+export default PortalForm;
